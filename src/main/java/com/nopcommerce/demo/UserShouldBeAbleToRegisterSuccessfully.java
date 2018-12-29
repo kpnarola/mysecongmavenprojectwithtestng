f1192d107d78285965a7f6128e3ad3569e4c926c
kpnarola <kpnarola@gmail.com>
2018-12-29 09:36:13 +0000

    Driver adding for cross browser testing

--- src/main/java/com/nopcommerce/demo/UserShouldBeAbleToRegisterSuccessfully.java
+++ src/main/java/com/nopcommerce/demo/UserShouldBeAbleToRegisterSuccessfully.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
